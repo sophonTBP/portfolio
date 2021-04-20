ac3221383decb38c8c2c2e95c8b17a215d37d021
Lucas Melin <65817583+sophonTBP@users.noreply.github.com>
2021-04-20 16:57:33 +0200

    api test1

--- scripts.js
+++ scripts.js
@@ -326,7 +326,7 @@ m = (m < 10) ? "0" + m : m;
   
  
   let test=''
-  window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Avranches,fr&lang=fr&units=metric&appid=400e81dce4ed66dfcdb4c54bd2348c76')
+  window.fetch('https://api.openweathermap.org/data/2.5/weather?q=Avranches,fr&lang=fr&units=metric&appid=400e81dce4ed66dfcdb4c54bd2348c76')
   .then(res=>res.json())
   .then(resJson =>{ document.querySelector('.temp').innerHTML = JSON.stringify(resJson.main.temp);
    document.querySelector('.value').innerHTML = JSON.stringify(resJson.weather[0].description);
